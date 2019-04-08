08c17431fc7e9442a2171e596c14296a28be59d1
monicaep <mep823@gmail.com>
2019-04-08 11:07:13 -0700

    Basic Node App: completed

---
+++ main.js
@@ -0,0 +1,14 @@
+const greetings = require("./greeting.js");
+const math = require("./math.js");
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+math.add(1, 2);
+
+math.multiply(2, 5);
---
+++ math.js
@@ -0,0 +1,7 @@
+exports.add = function(a, b) {
+  console.log(a + b);
+}
+
+exports.multiply = function(c, d) {
+  console.log(c * d);
+}
